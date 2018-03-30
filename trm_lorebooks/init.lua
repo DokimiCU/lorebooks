21d725b816ea5b4930d1634c5646f8642f0ccbd3
DokimiCU <DokimiCU@gmail.com>
2018-03-30 16:48:34 +1300

    Add files via upload

---
+++ trm_lorebooks/init.lua
@@ -0,0 +1,36 @@
+--This mod provides treasurer support so that lorebooks will spawn in the world.
+--note: this only registers the books as a treasure, it does not spawn them itself.
+
+-- name, rarity, preciousness, count
+-- set preciousness and rarity the same for all, otherwise too confusing
+
+local r = 0.4 --nice for them to be fairly common
+local p = 4
+local c = 1
+
+--Dokimi's books
+treasurer.register_treasure("lorebooks:the_minetestian_creed",r,p,c)
+treasurer.register_treasure("lorebooks:outside_awaits",r,p,c)
+treasurer.register_treasure("lorebooks:a_brief_history_of_the_universe",r,p,c)
+treasurer.register_treasure("lorebooks:homesteading_for_idiots",r,p,c)
+treasurer.register_treasure("lorebooks:the_minetestian_pantheon",r,p,c)
+treasurer.register_treasure("lorebooks:fora_ecstatica",r,p,c)
+treasurer.register_treasure("lorebooks:the_mese_mystery",r,p,c)
+treasurer.register_treasure("lorebooks:whats_in_a_name",r,p,c)
+treasurer.register_treasure("lorebooks:infinite_rebirth",r,p,c)
+treasurer.register_treasure("lorebooks:empire_shmempire",r,p,c)
+treasurer.register_treasure("lorebooks:imperial_decree_355iv",r,p,c)
+treasurer.register_treasure("lorebooks:the_mysterious_doktor_dokimi",r,p,c)
+treasurer.register_treasure("lorebooks:the_diary_of_kandy_kane_vol6",r,p,c)
+treasurer.register_treasure("lorebooks:the_revelation_of_wombat_the_wise",r,p,c)
+treasurer.register_treasure("lorebooks:palagon_the_fabled_city",r,p,c)
+treasurer.register_treasure("lorebooks:i_realm_to_realm_i",r,p,c)
+treasurer.register_treasure("lorebooks:travels_in_ecologia",r,p,c)
+treasurer.register_treasure("lorebooks:the_mikido_literary_foundation",r,p,c)
+treasurer.register_treasure("lorebooks:geography_101",r,p,c)
+treasurer.register_treasure("lorebooks:physics_101",r,p,c)
+treasurer.register_treasure("lorebooks:yobites_the_hidden_menace",r,p,c)
+treasurer.register_treasure("lorebooks:doktor_dokimis_research_notes_vol1",r,p,c)
+treasurer.register_treasure("lorebooks:doktor_dokimis_research_notes_vol2",r,p,c)
+treasurer.register_treasure("lorebooks:doktor_dokimis_research_notes_wormholes",r,p,c)
+treasurer.register_treasure("lorebooks:my_pockets_are_vast",r,p,c)
